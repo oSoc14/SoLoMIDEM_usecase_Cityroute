2e8d68fdfb8aefaa2e662e5ce6eff952ae2bdc52
alombide <alombide@vub.ac.be>
2013-09-20 17:04:37 +0200

    fixing auth issues

--- clientpage/js/auth/config.js
+++ clientpage/js/auth/config.js
@@ -4,5 +4,5 @@
 * a config file
 */
 
-var config_serverAddress = "cityroute.herokuapp.com";
+var config_serverAddress = "cityroute.herokuapp.com:12964";
 var config_WhatsNextAddress = "xxxxxxxxxxxxxxxxxxxxxxx";
