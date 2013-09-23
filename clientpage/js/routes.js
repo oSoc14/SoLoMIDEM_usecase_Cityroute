323676f016127bfe3262bdfc0e595c576b287c38
alombide <alombide@vub.ac.be>
2013-09-23 16:36:59 +0200

    Adding date switcher to routes view

--- clientpage/js/routes.js
+++ clientpage/js/routes.js
@@ -38,11 +38,11 @@ function showRoute ( spotID ){
 function onGetRoutes(data, textStatus, jqXHR) { 
     $("#routes").html("");
     // for each route
+    $("#routes").append("<p>View for date: <input type='text' id='current_datepicker' /></p>");
+    $( "#current_datepicker" ).datepicker();
     $("#routes").append("<div style='float:left;' ><input style='margin-right:50px;' type='button' value='Add new route' onclick='showRouteBuilder()'/> "  + 
         " Optimize Waypoints: <select id='optimizeSwitch'><option value='1'>On</option><option value='0'>Off</option></select></div>");
         $('#optimizeSwitch').switchify();
-    $("#routes").append("<br><p>View for date: <input type='text' id='current_datepicker' /></p><br>");
-    $( "#current_datepicker" ).datepicker();
     if (data.meta.code == 200) {
         $.each(data.response.routes, addRouteInformation);
     } else {
