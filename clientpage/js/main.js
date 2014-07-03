2bcf4b42219157694fbbaf172a5b414c9b2c6de9
thgh <thoogh@gmail.com>
2014-07-03 17:41:37 +0200

    Cleanup index.html Moved inline css and scripts Removed tables where possible Inserted p & label where possible Fixed html5 issues Removed all style attributes Put all "views" into consistent section tags Added titles to all sections
    Structure is now
    header
    >h1
    >nav
    main
    >container
    >>content
    >>aside

--- clientpage/js/main.js
+++ clientpage/js/main.js
@@ -1,1 +1,12 @@
-
+/* Enable some jQuery goodness */
+$("#sortable").sortable();
+$("#sortable").disableSelection();
+<!-- Refresh list to the end of sort to have a correct display -->
+$("#sortable").bind("sortstop", function(event, ui) {
+  $('#sortable').listview('refresh');
+});
+$(function() {
+  $("#tabs").tabs();
+});
+$("#datepicker_from_generate").datepicker();
+$("#datepicker_to_generate").datepicker();
