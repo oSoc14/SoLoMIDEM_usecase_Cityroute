98932e085da3069af1d5b48711814b2748b83117
thgh <thoogh@gmail.com>
2014-07-04 16:30:09 +0200

    Fix bug modernizr loading too soon

--- clientpage/js/CityRoute.js
+++ clientpage/js/CityRoute.js
@@ -9,31 +9,32 @@
 
 
 /**
-* on document ready: verify if a user is logged in, show and hide the correct views
-**/
-$(document).ready( function() {
-    // if a user is logged in
-    if ($.cookie("token") != null) {
-        changeView('geostatus');
-        getGeolocation();
-    }
-    // if a user is not is not logged in
-    else {
-        changeView('login')
-    }
-});
-/**
-* on Modernizr ready: use jquery-ui datepicker if datefield not supported
-**/
-Modernizr.load({
-  test: Modernizr.inputtypes.date,
-  nope: ['vendor/jquery/dist/jquery.min.js', 'vendor/jquery-ui/jquery-ui.min.js', 'vendor/jquery-ui/themes/flick/jquery-ui.min.css'],
-  complete: function () {
-    console.log('load datepicker');
-    $('input[type=date]').datepicker({
-      dateFormat: 'dd-mm-yy'
-    }); 
+ * on document ready: verify if a user is logged in, show and hide the correct views
+ **/
+$(document).ready(function() {
+  // if a user is logged in
+  if ($.cookie("token") != null) {
+    changeView('geostatus');
+    getGeolocation();
   }
+  // if a user is not is not logged in
+  else {
+    changeView('login')
+  }
+
+  /**
+   * on Modernizr ready: use jquery-ui datepicker if datefield not supported
+   **/
+  Modernizr.load({
+    test: Modernizr.inputtypes.date,
+    nope: ['vendor/jquery-ui/themes/flick/jquery-ui.min.css'],
+    complete: function() {
+      console.log('load datepicker');
+      $('input[type=date]').datepicker({
+        dateFormat: 'dd-mm-yy'
+      });
+    }
+  });
 });
 
 /**
